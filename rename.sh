6edb369d791533f8bc0e25a916f03e833dd02b50
Cristian Ciacu <d.c.ciacu@student.tudelft.nl>
2023-12-19 14:46:06 +0200

    chn

--- rename.sh
+++ rename.sh
@@ -11,16 +11,14 @@ problem_code=$(echo "$first_line" | awk -F'/' '{print $(NF-0)}')
 
 problem_number=$(echo "$first_line" | awk -F'/' '{print $(NF-1)}')
 
-echo ${problem_number}
-echo ${problem_code}
-
 # Create a new file with the extracted parts and copy the content of the .cpp file
 new_file="${problem_number}${problem_code}.cpp"
 echo "$(<"$cpp_file")" > "$new_file"
 
-echo "New file created: $new_file"
 
 # Upload to github
 git add ${new_file}
 git commit -m "Add ${first_line}"
 git push
+
+echo "New file created: $new_file and uploaded to github."
