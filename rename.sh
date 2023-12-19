51f186078600d8f12b7b5927bb04ca1d78680809
Cristian Ciacu <d.c.ciacu@student.tudelft.nl>
2023-12-19 02:17:19 +0200

    add script to rename the file to the name of the problem it solves.

---
+++ rename.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+cpp_file="main.cpp"
+
+# Get the first line of the .cpp file
+first_line=$(head -n 1 "$cpp_file")
+
+problem_code=$(echo "$first_line" | awk -F'/' '{print $(NF-0)}')
+
+problem_number=$(echo "$first_line" | awk -F'/' '{print $(NF-1)}')
+
+echo ${problem_number}
+echo ${problem_code}
+
+# Create a new file with the extracted parts and copy the content of the .cpp file
+new_file="${problem_number}${problem_code}.cpp"
+echo "$(<"$cpp_file")" > "$new_file"
+
+echo "New file created: $new_file"
